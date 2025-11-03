5b863a1a9eb0c91f8eff1f5155d150a1fae4eca4
rennegomes <renne.lace082@gmail.com>
2025-11-03 18:53:49 -0300

    dserializando objeto

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using exemplo_explorando.Models;
+using Newtonsoft.Json;
 
 // Pessoa p1 = new Pessoa(nome: "Renê", sobrenome: "Ferreira Dinis");
 // Pessoa p2 = new Pessoa(nome: "Elza", sobrenome: "Ferreira Dinis");
@@ -14,8 +15,16 @@
 // Colecoes colecoes = new Colecoes();
 // colecoes.NumerosEmCascata();
 
-using Newtonsoft.Json;
-Vendas v1 = new Vendas(1, "teclado", 500.00M);
-string serializado = JsonConvert.SerializeObject(v1, Formatting.Indented);
-File.WriteAllText("Arquivos/vendas.json", serializado);
-Console.WriteLine(serializado);
+// using Newtonsoft.Json;
+// DateTime dataAtual = DateTime.Now;
+// Vendas v1 = new Vendas(1, "teclado", 500.00M, dataAtual);
+// string serializado = JsonConvert.SerializeObject(v1, Formatting.Indented);
+// File.WriteAllText("Arquivos/vendas.json", serializado);
+// Console.WriteLine(serializado);
+
+string conteudoArquivo = File.ReadAllText("Arquivos/vendas.json");
+List<Vendas> listaVenda = JsonConvert.DeserializeObject<List<Vendas>>(conteudoArquivo);
+foreach (Vendas venda in listaVenda)
+{
+    Console.WriteLine($"Id: {venda.Id}\nProduto: {venda.Produto}\nPreco: {venda.Preco}\nData: {venda.DataVenda.ToString("dd/MM/yyyy HH:mm")}");
+}
--- obj/Debug/net9.0/exemplo-explorando.AssemblyInfo.cs
+++ obj/Debug/net9.0/exemplo-explorando.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("exemplo-explorando")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c73fed0a09ef94c13d3ce22d97d7912d515f2e2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7406b04680d1abaa9b3621cfc80467d49b548d1c")]
 [assembly: System.Reflection.AssemblyProductAttribute("exemplo-explorando")]
 [assembly: System.Reflection.AssemblyTitleAttribute("exemplo-explorando")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
